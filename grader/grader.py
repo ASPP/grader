b1d4188a185b0843308beaa9a46578ce225c78ab
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-02-08 12:05:36 +0100

    grader: implement ranking

--- grader/grader.py
+++ grader/grader.py
@@ -3,6 +3,7 @@ import traceback
 import csv
 import collections
 import textwrap
+import pprint
 
 import cmd_completer
 import vector
@@ -24,7 +25,30 @@ python: {p.python}
 open source: {p.open_source}{open_source_description}
 '''
 
-score_range = (-1, 0, 1)
+SCORE_RANGE = (-1, 0, 1)
+
+PROGRAMMING_RATING = {
+    'Novice': 0,
+    'Advanced beginner': 0,
+    'Competent': 0.7,
+    'Expert': 0.9,
+    'Proficient': 1,
+}
+
+OPEN_SOURCE_RATING = {
+    'Never used': -1,
+    'User': 0,
+    'Minor contributions': 0.5,
+    'Major contributions': 1,
+    'Project membership': 1,
+}
+
+# FIXME: should be changed to the real number of times
+APPLIED_RATING = {
+    'N': 0, # ???
+    'No': 0,
+    'Yes': 1,
+}
 
 class Grader(cmd_completer.Cmd_Completer):
     prompt = 'grader> '
@@ -36,6 +60,10 @@ class Grader(cmd_completer.Cmd_Completer):
 
         self.applications = applications
         self.formula = None
+        self.programming_rating = PROGRAMMING_RATING
+        self.open_source_rating = OPEN_SOURCE_RATING
+        self.applied_rating = APPLIED_RATING
+        self.accept_count = 30
 
     def do_dump(self, arg):
         for p in self.applications:
@@ -66,13 +94,33 @@ class Grader(cmd_completer.Cmd_Completer):
 
         if opts.what == 'formula':
             if opts.args:
-                self.formula = ' '.join(opts.args)
+                # check syntax
+                formula = ' '.join(opts.args)
+                compile(formula, '--formula--', 'eval')
+                self.formula = formula
             printf('formula is {}', self.formula)
             return
 
         for person in self.applications:
             self._grade(person, opts.what)
 
+    rate_options = cmd_completer.ModArgumentParser('rate')\
+        .add_argument('what',
+                      choices=['programming', 'open_source', 'applied'])\
+        .add_argument('args', nargs='*')
+
+    @set_completions('programming', 'open_source', 'applied')
+    def do_rate(self, arg):
+        "Set rating for activity to some value"
+        opts = self.rate_options.parse_args(arg.split())
+        varname = opts.what + '_rating'
+        dict = getattr(self, varname)
+        if not opts.args:
+            pprint.pprint(dict)
+        else:
+            how = ' '.join(opts.args[:-1])
+            value = float(opts.args[-1])
+            dict[how] = value
 
     def _grade(self, person, what):
         assert what in {'motivation', 'cv'}, what
@@ -82,16 +130,16 @@ class Grader(cmd_completer.Cmd_Completer):
         printf('{line}\n{}\n{line}', wrap_paragraphs(text), line='-'*70)
         printf('Old score was {}', old_score)
         while True:
-            choice = input('Your choice {} [{}]? '.format(score_range, default))
+            choice = input('Your choice {} [{}]? '.format(SCORE_RANGE, default))
             if choice == '':
                 choice = default
             if choice == '+':
-                choice = score_range[-1]
+                choice = SCORE_RANGE[-1]
             if choice == '-':
-                choice = score_range[0]
+                choice = SCORE_RANGE[0]
             try:
                 choice = int(choice)
-                if choice not in score_range:
+                if choice not in SCORE_RANGE:
                     raise ValueError('illegal value: {}'.format(choice))
             except ValueError as e:
                 print(e)
@@ -99,6 +147,63 @@ class Grader(cmd_completer.Cmd_Completer):
                 break
         setattr(person, what + '_score', choice)
         printf('{} score set to {}', what, choice)
+
+    def do_rank(self, arg):
+        "Order applications by rank"
+        if self.formula is None:
+            raise ValueError('formula not set yet')
+
+        for person in self.applications:
+            person.score = grade_person(person, self.formula,
+                                        self.programming_rating,
+                                        self.open_source_rating,
+                                        self.applied_rating)
+        ranked = sorted(self.applications, key=lambda p: p.score, reverse=True)
+        for rank, person in enumerate(ranked):
+            if rank == self.accept_count:
+                print('-' * 70)
+            printf('{: 2} {p.score:2.1f} {p.name} {p.lastname} <{p.email}>',
+                   rank, p=person)
+
+def grade_person(person, formula,
+                 programming_rating, open_source_rating, applied_rating):
+    "Apply formula to person and return score"
+    vars = {}
+    for type in 'programming', 'open_source', 'applied':
+        dict = locals().get(type + '_rating')
+        key = getattr(person, type)
+        key = key.partition('(')[0].partition('/')[0].strip()
+                    # remove explanation in () or after /
+        try:
+            value = dict[key]
+        except KeyError:
+            raise ValueError('{} not rated for {}'.format(key, type))
+                       # from None, when implemented!
+        vars[type] = value
+    try:
+        person.motivation_score
+        person.cv_score
+    except AttributeError as e:
+        #raise ValueError('{p.name} {p.lastname}: {e}'.format(p=person, e=e))
+        import random
+        person.motivation_score = random.choice(SCORE_RANGE)
+        person.cv_score = random.choice(SCORE_RANGE)
+
+    vars.update(born=person.born, # if we decide to implement agism
+                gender=person.gender, # if we decide, ...
+                                      # oh we already did
+                female=(person.gender == 'Female'),
+                nation=person.nation,
+                country=person.country,
+                motivation=person.motivation_score,
+                cv=person.cv_score,
+                email=person.email, # should we discriminate against gmail?
+                )
+    try:
+        score = eval(formula, vars, {})
+    except (NameError, TypeError) as e:
+        raise ValueError('formula failed: {}'.format(e))
+    return score
 
 def wrap_paragraphs(text):
     paras = text.strip().split('\n\n')
@@ -144,9 +249,9 @@ def main(argv0, *args):
             except KeyboardInterrupt:
                 print
             except SyntaxError as e:
-                log.exception('bad command: %s', e)
+                print('bad command: %s', e)
             except ValueError as e:
-                log.exception('bad value: %s', e)
+                print('bad value: %s', e)
                 traceback.print_exc()
     else:
         input = cmd_completer.InputFile(sys.stdin)
