68dacc518ff4691fb4385428ea02eab4d7a10788
Tiziano Zito <opossumnano@gmail.com>
2015-05-22 00:14:56 +0200

    WIP: improved convergence criteria and more refactoring

--- plugins/create_groups/create_groups.py
+++ plugins/create_groups/create_groups.py
@@ -19,9 +19,13 @@ SKILL_WEIGHTS = collections.OrderedDict([
 # Probability of accepting one step in the wrong direction
 # in the stochastic algorithm
 REJECTION_PROBABILITY = 1.
-
-# Print a lot of debugging output
-DEBUG = True
+# Minimum number of independent trials to run
+MIN_TRIALS = 5
+# Maximum number of rejected step in the optimization routine before
+# declaring convergence
+MAX_REJECTIONS = 200
+# Relative and absolute tolerance to consider two values of energy equal
+RTOL, ATOL = 0.001, 1e-10
 
 ### DO NOT NEED TO CHANGE BELOW THIS LINE ###
 
@@ -32,10 +36,6 @@ NSTUDENTS = int(config['formula']['accept_count'])
 LABELS = config.sections['labels']
 # How many students in a group
 GSIZE = config['groups_parameters']['group_size']
-# How many independent trials to run of the stochastic algorithm
-TRIALS = config['groups_parameters']['number_trials']
-# How long to run each independent trial
-REPETITIONS = config['groups_parameters']['repetitions']
 # Random seed (expected to be a UTF8 string, typically "City YEAR")
 RANDOM_SEED = config['groups_random_seed']['seed']
 
@@ -57,7 +57,7 @@ RANDOM_SEED = bytes(RANDOM_SEED, encoding='utf8')
 # of trials to avoid an integer overflow down the road).
 # The result is one nice random seed.
 RANDOM_SEED = np.fromstring(hashlib.sha512(RANDOM_SEED).digest(),
-                            dtype=np.uint64).sum()//np.uint64(TRIALS)
+                            dtype=np.uint64).sum()
 
 def debug(*args, **kwargs):
     if DEBUG:
@@ -83,8 +83,7 @@ def extract_data():
     """
     # transform confirmed list into useful format for generating groups
     # a single entry will contain
-    # (idx, gender, python, programming, vcs, open source) ratings as a tuple
-    # the idx is necessary to keep track of the people's names
+    # the idx and the ordered dict are necessary to keep track of names
     people = collections.OrderedDict()
     for idx, person in enumerate(participants()):
         rates = [idx]
@@ -107,39 +106,32 @@ def group(i):
     return slice(i * GSIZE, (i + 1) * GSIZE)
 
 
-def print_groups(data, energy, names):
+def print_groups(data, energy, people):
     """prints dataset sorted into groups of size GSIZE, calculates energy of
     solution and average ratings for all groups"""
+    names = {people[name][0]:name for name in people}
 
-    debug('energy: %.4f' % (energy(data)))
+    debug('energy:', energy(data))
     for i in range(NGROUPS):
-        debug('#########################')
         debug('Group %d:' % (i))
-        debug('Ratings:')
-        debug(np.round(data[group(i), 1:], 2))
-        debug('-------------------------')
         debug('Group average:')
-        debug(np.round([x for x in np.mean(data[group(i), 1:],
-                                        axis=0)], 2))
+        debug(np.round([x for x in data[group(i), 1:].mean(axis=0)], 2))
         print([names[int(k)] for k in data[group(i), 0]])
-        debug('#########################')
 
     debug('Target averages:')
-    debug(np.round(np.mean(data[:, 1:], axis=0), 2))
+    debug(np.round(data[:, 1:].mean(axis=0), 2))
     debug('#########################')
     debug('Rel. deviation from target averages:')
     for i in range(NGROUPS):
         debug(np.round((np.array([round(x, 2) \
-                    for x in np.mean(data[group(i), 1:], axis=0)]) -
-                    np.mean(data[:, 1:], axis=0)) / np.mean(data[:, 1:],
-                                                             axis=0), 2))
+                    for x in data[group(i), 1:].mean(axis=0)]) -
+                    data[:, 1:].mean(axis=0)) / data[:, 1:].mean(axis=0), 2))
     debug('-------------------------')
     debug('Rel. deviation from target standard deviations:')
     for i in range(NGROUPS):
         debug(np.round((np.array([round(x, 2) \
-                    for x in np.std(data[group(i), 1:], axis=0)]) -
-                    np.std(data[:, 1:], axis=0)) / np.std(data[:, 1:],
-                                                           axis=0), 2))
+                    for x in data[group(i), 1:].std(axis=0)]) -
+                    data[:, 1:].std(axis=0)) / data[:, 1:].std(axis=0), 2))
 
 
 def optimize(data, energy, p=REJECTION_PROBABILITY):
@@ -149,9 +141,14 @@ def optimize(data, energy, p=REJECTION_PROBABILITY):
     their position is swapped and the change in energy is calculated.
     If the energy is lower afterwards, keep the change, otherwise
     reject the change with a probability p.
-    a function which calculates the energy must be provided."""
+    A function which calculates the energy must be provided."""
 
-    for i in range(REPETITIONS):
+    rejected = 0
+    count = 0
+
+    #for i in range(REPETITIONS):
+    while True:
+        count += 1
         # pick two random students
         # make sure the two picked ones are not
         # in the same group
@@ -168,12 +165,22 @@ def optimize(data, energy, p=REJECTION_PROBABILITY):
         if E_before < E_after and (p==1 or p > np.random.rand()):
             # reject the change
             data[idx] = data[idx[::-1]]
+            rejected += 1
+        elif np.isclose(E_before, E_after, rtol=RTOL, atol=ATOL):
+            # there was a minimal improvement but let's count it as a rejection
+            rejected += 1
+        else:
+            # this was a good step, let's reset the rejection counter
+            rejected = 0
+        if rejected > MAX_REJECTIONS:
+            # we had enough rejections, no point in optimizing further
+            return count
 
 
 def energy_mudeviation(skill):
     """Penalize deviation of a group from the mean over all groups for a certain
     skill."""
-    return np.std([np.mean(skill[group(i)]) for i in range(NGROUPS)])
+    return np.std([skill[group(i)].mean() for i in range(NGROUPS)])
 
 
 def energy_nonuniform(skill):
@@ -188,7 +195,7 @@ def energy_nonuniform(skill):
     # this is not used right now, as it doesn't seem to make a difference
     # most probably because it is too small of a term:
     # std(std) vs std(mean)
-    return np.std([np.std(skill[group(i)]) for i in range(NGROUPS)])
+    return np.std([skill[group(i)].std() for i in range(NGROUPS)])
 
 
 def energy(data):
@@ -203,7 +210,6 @@ def energy(data):
 def main():
     people = extract_data()
     in_data = np.array(list(people.values()))
-    E0_trial = []
     E_trial = []
     data_trial = []
 
@@ -211,33 +217,32 @@ def main():
     np.random.seed(RANDOM_SEED)
 
     print('Running trials...')
-    for seed in np.arange(1, TRIALS+1, dtype=np.uint64):
-        # give a bit of a progress report
-        print('Trial: %d/%d'%(seed,TRIALS)+20*'\b', end='', flush=True)
+    trial = 0
+    while True:
+        trial += 1
         # create different initial condition for every trial
-        np.random.seed(seed * RANDOM_SEED)
+        np.random.seed(np.uint64(trial) * RANDOM_SEED)
         # IMPORTANT: data gets modified in place in the optimize
         # function! Do not generate copies here!
         data = np.random.permutation(in_data)
-        # store the starting energy of the trial
-        if DEBUG: E0_trial.append(energy(data))
-        optimize(data, energy)
+        count = optimize(data, energy)
+        # final energy
+        E_final = energy(data)
         # store the final energy of the trial
-        E_trial.append(energy(data))
+        E_trial.append(E_final)
         # store the final configuration of the trial
         data_trial.append(data)
+        # give a bit of a progress report
+        print('Trial #%d(%d):'%(trial, count), E_final)
+        # stop if we have collected enough trials
+        # we stop only if we have run at least MIN_TRIALS and
+        # if we did not improve much in this last step
+        if trial >= MIN_TRIALS and np.isclose(E_final, min(E_trial[:-1]), rtol=RTOL, atol=ATOL):
+            print('Converged!')
+            break
 
-    print()
-    debug('initial energy of all trials:')
-    debug('mean: %.4f, std: %.4f' % (np.mean(E0_trial),
-                                  np.std(E0_trial)))
-    debug('final energy of all trials:')
-    debug('mean: %.4f, std: %.4f' % (np.mean(E_trial), np.std(E_trial)))
-    print('optimal group distribution:')
-    pos = np.argsort(E_trial)
-    names = {people[name][0]:name for name in people}
-
-    print_groups(data_trial[pos[0]], energy, names)
+    # extract the better position
+    print_groups(data_trial[np.argmin(E_trial)], energy, people)
     # TODO write data in csv file for later use
 
 main()
