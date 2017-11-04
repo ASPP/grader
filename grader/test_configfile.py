ae09d831914ff665fcf2783a0e4006f638b52f0e
Pietro Berkes <pberkes@enthought.com>
2017-11-04 22:44:55 +0100

    Test `ConfigFile` functionality

--- grader/test_configfile.py
+++ grader/test_configfile.py
@@ -3,14 +3,16 @@ from io import StringIO
 from configfile import ConfigFile
 
 
-def test_configfile_init():
-    config_string = StringIO("""
-    [programming_rating]
-    competent = 1.0
-    expert = 0.0
-    novice = 0.0
-    """)
+CONFIG_STRING_MINIMAL = """
+[programming_rating]
+competent = 1.0
+expert = 0.0
+novice = 0.0
+"""
 
+
+def test_configfile_init():
+    config_string = StringIO(CONFIG_STRING_MINIMAL)
     config = ConfigFile(
         config_string,
         programming_rating=float,
@@ -27,3 +29,46 @@ def test_configfile_init():
     assert config.cp.has_section('python_rating')
     assert 'python_rating' in config.sections
     assert len(list(config.sections['python_rating'].items())) == 0
+
+
+def test_setting_section_updates_configfile():
+    config_string = StringIO(CONFIG_STRING_MINIMAL)
+    config = ConfigFile(config_string, programming_rating=float)
+    section = config.sections['programming_rating']
+
+    # Update section value
+    section['novice'] = -1.0
+    assert config.cp.get('programming_rating', 'novice') == '-1.0'
+
+    # Add section value
+    section['guru'] = 2.0
+    assert config.cp.get('programming_rating', 'guru') == '2.0'
+
+
+def test_configfile_save(tmpdir):
+    config_string = StringIO(CONFIG_STRING_MINIMAL)
+
+    config = ConfigFile(
+        config_string,
+        programming_rating=float,
+        python_rating=float,
+    )
+
+    # Make changes
+    config.sections['programming_rating']['novice'] = -1.0
+    config.sections['python_rating']['competent'] = 100.0
+
+    # Save changes
+    config_file = tmpdir.join("temp.conf")
+    config.save(config_file.strpath)
+
+    # Reload
+    with config_file.open() as f:
+        config_reread = ConfigFile(
+            f,
+            programming_rating=float,
+            python_rating=float,
+        )
+
+        assert config_reread.sections['programming_rating']['novice'] == -1.0
+        assert config_reread.sections['python_rating']['competent'] == 100.0
