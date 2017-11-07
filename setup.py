c0f1aa8e1cb5db5cd44f90058d3c027fa6f6b1c0
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-11-07 12:16:36 +1100

    Modify setup.py appropriately

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-"""A setuptools based setup module.
+"""Grader: a Python module and command-line utility to grade applications.
 
-See:
+This setup.py is based on the template at the PyPA sample project. See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
@@ -14,26 +14,26 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='grader',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version='0.1.0',
 
-    description='A sample Python project',
+    description='A Python module and command-line utility to grade applications.',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/ASPP/grader',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author='ASPP Contributors',
+    author_email='python@g-node.org',
 
     # Choose your license
     license='MIT',
@@ -55,16 +55,15 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='grading applications',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -78,36 +77,39 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=[],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'dev': [],
+        'test': ['pytest', 'coverage'],
     },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
+    # Check packaging.python.org for details on this format
     package_data={
-        'sample': ['package_data.dat'],
+        #'sample': ['package_data.dat'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    #data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
+    # entry_points = { console_scripts : [
+    # script_name=package_name.module:function_name ] }
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'grader=grader.grader:main',
         ],
     },
 )
