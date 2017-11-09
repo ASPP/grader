fc598542fdcee86423689a3a32925e5bb71faef9
Pietro Berkes <pberkes@enthought.com>
2017-11-09 20:50:14 +0100

    Simplify `setup.py`

--- setup.py
+++ setup.py
@@ -1,54 +1,48 @@
 # -*- coding: utf-8 -*-
-import ast
 import os
+
 from setuptools import setup, find_packages
+
+from grader import (
+    __author__,
+    __description__,
+    __license__,
+    __name__,
+    __url__,
+    __version__,
+)
 
 # this file is used to pick the relevant metadata for setup.py
 INITFILE = os.path.join('grader', '__init__.py')
 # the directory we are in
 CWD = os.path.abspath(os.path.dirname(__file__))
 
-def parse_keyword(key):
-    """Get metadata from grader/__init__.py using an AST"""
-    with open(os.path.join(CWD, INITFILE), encoding='utf-8') as f:
-        ast_tree = ast.parse(f.read())
-        for node in ast.walk(ast_tree):
-            if type(node) is ast.Assign:
-                try:
-                    if node.targets[0].id == key:
-                        return node.value.s
-                except:
-                    pass
-    # return "not available" if we didn't find the variable
-    return 'N/A'
-
-# pick the relevant keywords from the __init__.py file
-metadata_vars = ('name', 'version', 'description', 'author', 'license', 'url')
-metadata = dict((var, parse_keyword('__%s__'%var)) for var in metadata_vars)
-
 # Get the long description from the README file
 with open(os.path.join(CWD, 'README.md'), encoding='utf-8') as f:
-    metadata['long_description'] = f.read()
+    long_description = f.read()
 
 setup(
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    version=__version__,
+    author=__author__,
+    description=__description__,
+    long_description=long_description,
+    license=__license__,
+    name=__name__,
+    url=__url__,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa
         'Programming Language :: Python :: 3',
     ],
     keywords='grading applications',
     packages=find_packages(),
     install_requires=['pytest'],
-    package_data={
-        #'sample': ['package_data.dat'],
-    },
     entry_points={
         'console_scripts': [
             'grader=grader.grader:main',
         ],
     },
-    **metadata
 )
